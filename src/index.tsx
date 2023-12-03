1819f635f6e81678523c1d5bf40a3f16ee598d0d
Luis von Rondow <luiseduardorondow@gmail.com>
2023-12-03 18:15:39 +0000

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import { html } from "@elysiajs/html";
+import Elysia from "elysia";
+import * as elements from "typed-html";
+
+new Elysia()
+  .use(html())
+  .get("/", ({ html }) =>
+    html(
+      <HomeLayout>
+        <h1>Oiii</h1>
+      </HomeLayout>
+    )
+  )
+  .listen(3000);
+
+const HomeLayout = ({ children }: elements.Children) => `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://unpkg.com/htmx.org@1.9.5"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <style>
+      * {
+        font-family: 'Inter', sans-serif;
+      }
+    </style>
+    <title>RONDOW HTMX</title>
+  </head>
+  <body class="h-screen w-screen flex flex-col gap-3 items-center justify-center">
+    ${children}
+  </body>
+  </html>
+`;
